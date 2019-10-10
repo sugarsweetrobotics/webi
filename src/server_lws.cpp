8cd7bac69e3294d7e5dde5e7200221183dc3c364
Yuki Suga <ysuga@sugarsweetrobotics.com>
2019-10-10 10:36:32 +0900

    update libwebsocket submodule

--- src/server_lws.cpp
+++ src/server_lws.cpp
@@ -5,6 +5,7 @@
 
 #include "webi/webi.h"
 
+#include <libwebsockets.h>
 using namespace webi;
 
 /*
@@ -22,9 +23,17 @@ void apply(httplib::Response &response, webi::Response &&r)
 }
 */
 
+
+
+
 class ServerImpl : public Server{
 private:
+  std::string origin_;
+  struct lws_http_mount mount_;
 
+  struct lws_context_creation_info info_;
+
+  struct lws_context *context_;
 public:
   ServerImpl();
   virtual ~ServerImpl();
@@ -45,7 +54,27 @@ public:
 };
 
 
-ServerImpl::ServerImpl() {
+static const struct lws_http_mount _default_mount = {
+  /* .mount_next */NULL,/* linked-list "next" */
+  /* .mountpoint */"/",/* mountpoint URL */
+  /* .origin */"./www", /* serve from dir */
+  /* .def */"index.html",/* default filename */
+  /* .protocol */NULL,
+  /* .cgienv */NULL,
+  /* .extra_mimetypes */NULL,
+  /* .interpret */NULL,
+  /* .cgi_timeout */0,
+  /* .cache_max_age */0,
+  /* .auth_mask */0,
+  /* .cache_reusable */0,
+  /* .cache_revalidate */0,
+  /* .cache_intermediaries */0,
+  /* .origin_protocol */LWSMPRO_FILE,/* files in a dir */
+  /* .mountpoint_len */1,/* char count */
+  /* .basic_auth_login_file */NULL,
+};
+
+ServerImpl::ServerImpl() : mount_(_default_mount) {
 }
 
 ServerImpl::ServerImpl(ServerImpl&& server) {
@@ -56,11 +85,13 @@ ServerImpl::~ServerImpl() {
 }
 
 void ServerImpl::baseDirectory(const std::string& path) {
-
+  origin_ = path;
+  mount_.origin = origin_.c_str();
 }
 
 void ServerImpl::response(const std::string& path, const std::string& method, const std::string& contentType, const std::string& response) {
   if (method == "GET") {
+    
   }
 }
 
@@ -72,8 +103,34 @@ void ServerImpl::response(const std::string& path, const std::string& method, co
 
 }
 
+static int _interrupted = 0;
+static void _sigint_handler(int sig)
+{
+  _interrupted = 1;
+}
+
 void ServerImpl::runForever(const int32_t port /*=8080*/) {
 
+  signal(SIGINT, _sigint_handler);
+
+  memset(&info_, 0, sizeof info_); /* otherwise uninitialized garbage */
+  info_.port = port;
+  info_.mounts = &mount_;
+  info_.error_document_404 = "/404.html";
+  info_.options = LWS_SERVER_OPTION_HTTP_HEADERS_SECURITY_BEST_PRACTICES_ENFORCE;
+  
+  context_ = lws_create_context(&info_);
+  if (!context_) {
+    lwsl_err("lws init failed\n");
+    return;
+  }
+
+  int n = 0;
+  while (n >= 0 && _interrupted == 0) {
+    n = lws_service(context_, 0);
+  }
+
+  lws_context_destroy(context_);
 }
 
 void ServerImpl::runBackground(const int32_t port /*=8080*/) {
